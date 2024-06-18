bb3d02c6b4f72a3b27cbc56ff83b07c63c0c2e6f
limhizy15 <limhizy15@ajou.ac.kr>
2024-06-18 14:48:59 +0900

    feat: PostCard 컴포넌트 수정 및 /posts 페이지에도 적용

---
+++ src/components/ui/Chip.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+type Chip = {
+  text: string;
+  theme: 'default' | 'info';
+  onClickCallback: () => void;
+};
+
+type ChipColorSet = {
+  [key in Chip['theme']]: {
+    backgroundColor: string;
+    borderColor: string;
+  };
+};
+
+const colors: ChipColorSet = {
+  default: {
+    backgroundColor: 'gray-900/10',
+    borderColor: 'gray-300',
+  },
+  info: {
+    backgroundColor: 'blue-500',
+    borderColor: 'blue-300',
+  },
+};
+
+export default function Chip({ text, theme, onClickCallback }: Chip) {
+  return (
+    <button
+      onClick={onClickCallback}
+      className={`relative  grid select-none items-center whitespace-nowrap rounded-sm bg-${colors[theme].backgroundColor} py-1 px-2 text-xs font-semi-bold uppercase text-gray-900 border-2 border-${colors[theme].borderColor}`}
+    >
+      {text}
+    </button>
+  );
+}
