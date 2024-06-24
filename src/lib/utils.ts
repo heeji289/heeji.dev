a93fbdce1d854959486c0b7911167b64ce1b144c
limhizy15 <limhizy15@ajou.ac.kr>
2024-06-24 15:20:28 +0900

    shadcn/ui 세팅

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export function convertToUniqArray<T>(array: T[]) {
   return Array.from(new Set(array));
 }
