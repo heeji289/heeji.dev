7c2fc7b8ed1a5cdeb547a7a992e38ee1376ce397
limhizy15 <limhizy15@ajou.ac.kr>
2024-06-25 18:14:56 +0900

    [#11] feat: 댓글창 추가

---
+++ src/components/Giscus.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+
+export default function Giscus() {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const theme = 'light';
+
+  useEffect(() => {
+    if (!ref.current || ref.current.hasChildNodes()) return;
+
+    const scriptElem = document.createElement('script');
+    scriptElem.src = 'https://giscus.app/client.js';
+    scriptElem.async = true;
+    scriptElem.crossOrigin = 'anonymous';
+
+    scriptElem.setAttribute('data-repo', 'heeji289/nextjs-blog');
+    scriptElem.setAttribute('data-repo-id', 'R_kgDOMEPXTw');
+    scriptElem.setAttribute('data-category', 'General');
+    scriptElem.setAttribute('data-category-id', 'DIC_kwDOMEPXT84CgXIB');
+    scriptElem.setAttribute('data-mapping', 'og:title');
+    scriptElem.setAttribute('data-strict', '0');
+    scriptElem.setAttribute('data-reactions-enabled', '1');
+    scriptElem.setAttribute('data-emit-metadata', '0');
+    scriptElem.setAttribute('data-input-position', 'bottom');
+    scriptElem.setAttribute('data-theme', 'preferred_color_scheme');
+    scriptElem.setAttribute('data-lang', 'ko');
+    scriptElem.setAttribute('data-loading', 'lazy');
+
+    ref.current.appendChild(scriptElem);
+  }, [theme]);
+
+  // https://github.com/giscus/giscus/blob/main/ADVANCED-USAGE.md#isetconfigmessage
+  useEffect(() => {
+    const iframe = document.querySelector<HTMLIFrameElement>(
+      'iframe.giscus-frame'
+    );
+    iframe?.contentWindow?.postMessage(
+      { giscus: { setConfig: { theme } } },
+      'https://giscus.app'
+    );
+  }, [theme]);
+
+  return <section ref={ref} />;
+}
