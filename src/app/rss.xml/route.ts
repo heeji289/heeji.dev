f59b4cec04cc37f27bd28de6f217ed6ffe3f1b4a
limhizy15 <limhizy15@ajou.ac.kr>
2024-07-01 14:31:47 +0900

    [#31] feat: rss.xml 생성 라우트 추가

---
+++ src/app/rss.xml/route.ts
@@ -0,0 +1,34 @@
+import { getAllPosts } from '@/service/post';
+import RSS from 'rss';
+
+export async function GET() {
+  const posts = await getAllPosts();
+
+  const feed = new RSS({
+    title: 'heeji.dev',
+    description: '임희지의 개발 블로그. 프론트엔드 개발 관련 기록을 남깁니다.',
+    site_url: 'https://heeji.dev',
+    feed_url: `https://heeji.dev/rss.xml`,
+    copyright: `${new Date().getFullYear()} heeji.dev`,
+    language: 'ko',
+    pubDate: new Date(),
+  });
+
+  posts?.forEach((post) => {
+    feed.item({
+      title: post.title,
+      guid: `https://heeji.dev/${post.id}`,
+      url: `https://heeji.dev/${post.id}`,
+      date: post.date, // TODO: 노션DB에 updateDate추가하고 변경할 것
+      description: post?.slug ?? '',
+      author: '임희지',
+      categories: post.tags || [],
+    });
+  });
+
+  return new Response(feed.xml({ indent: true }), {
+    headers: {
+      'Content-Type': 'application/atom+xml; charset=utf-8',
+    },
+  });
+}
