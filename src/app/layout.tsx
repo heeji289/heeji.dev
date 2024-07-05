2b0d1aed301066a6f4c9e60034b9229408c3c049
limhizy15 <limhizy15@ajou.ac.kr>
2024-07-05 14:25:34 +0900

    hotfix: GA 수집 코드 위치 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,22 +56,20 @@ export default function RootLayout({
           `,
           }}
         />
-
+      </head>
+      <body className={`bg-background text-black antialiased dark:text-white`}>
         <Script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
-
         <Script id='google-analytics'>
           {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', ${'${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'});
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
           `}
         </Script>
-      </head>
-      <body className={`bg-background text-black antialiased dark:text-white`}>
         <section className='mx-auto max-w-3xl xl:mx-w-[50rem] xl:px-8'>
           <div className='box-border flex h-full flex-col justify-between'>
             <Header2 />
